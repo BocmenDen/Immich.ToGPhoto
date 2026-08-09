0cccf0a07606b53ae57f5c29858ce50d247e06be
BocmenDen <bocmen.den@gmail.com>
2026-08-09 11:57:21 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,12 @@ set -e
 cd /app/python
 uvicorn main:app --host 0.0.0.0 --port 2282 &
 
+
+echo "Waiting for Uvicorn to start..."
+until (echo > /dev/tcp/localhost/2282) &>/dev/null; do
+    sleep 1
+done
+echo "Uvicorn is ready!"
+
 cd /app/dotnet
 exec dotnet Immich.ToGPhoto.App.dll
