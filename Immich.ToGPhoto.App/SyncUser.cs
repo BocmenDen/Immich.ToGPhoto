7c42d06168cc4cae988d27a11f0fd6b3ea34d27f
BocmenDen <bocmen.den@gmail.com>
2026-08-09 11:51:41 +0300

    Update SyncUser.cs

--- Immich.ToGPhoto.App/SyncUser.cs
+++ Immich.ToGPhoto.App/SyncUser.cs
@@ -9,7 +9,7 @@ using System.Text;
 
 namespace Immich.ToGPhoto.App
 {
-    public class SyncUser : IDisposable
+    public class SyncUser
     {
         private readonly static DateTime TrashedAfter = DateTime.Parse("1970-01-01T00:00:00.000Z");
         private const int CHANK_SIZE = 1000;
@@ -18,7 +18,6 @@ namespace Immich.ToGPhoto.App
         public readonly string Key = null!;
         private readonly GPMCClient _gpmcClient = null!;
         private readonly ImmichClient _immichClient = null!;
-        private readonly SyncDB _syncDB = null!;
         private readonly SyncUserConfig _config = null!;
 
         private SyncUser() { }
@@ -28,7 +27,6 @@ namespace Immich.ToGPhoto.App
             _gpmcClient = gpmcClient;
             _immichClient = immichClient;
             Key = $"{_gpmcClient.Key}_{immichClient.Key}";
-            _syncDB = new SyncDB(Key);
             Name = name;
             _config = syncUserConfig ?? new();
         }
@@ -36,20 +34,21 @@ namespace Immich.ToGPhoto.App
         public async Task SyncNewPhotos(ILogger logger)
         {
             await _gpmcClient.UpdateCacheAsync();
-            await LoadPhotos(logger);
-            await DeletePhotos(logger);
-            await DeleteAnotherPhotos(logger);
+            using var db = GetDB();
+            await LoadPhotos(db, logger);
+            await DeletePhotos(db, logger);
+            await DeleteAnotherPhotos(db, logger);
         }
 
         /// <summary>
         /// Удаление фото которые не были загружены в Immich, но есть в Google Photos
         /// </summary>
-        private async Task DeleteAnotherPhotos(ILogger logger)
+        private async Task DeleteAnotherPhotos(SyncDB db, ILogger logger)
         {
             List<string> toDeleate = [];
             await foreach (var chank in _gpmcClient.GetKeys().Chunk(CHANK_SIZE))
             {
-                var findElems = await _syncDB.SyncItems.Where(x => chank.Contains(x.GoogleKey)).ToListAsync();
+                var findElems = await db.SyncItems.Where(x => chank.Contains(x.GoogleKey)).ToListAsync();
                 var deleteElems = chank.Except(findElems.Select(x => x.GoogleKey));
                 toDeleate.AddRange(deleteElems);
             }
@@ -63,12 +62,12 @@ namespace Immich.ToGPhoto.App
         /// <summary>
         /// Гарантия что фото в корзине будут удалены из GPhoto
         /// </summary>
-        private async Task DeletePhotos(ILogger logger)
+        private async Task DeletePhotos(SyncDB db, ILogger logger)
         {
             var allAssetsImmich = _immichClient.SearchAllAssetsAsync(new MetadataSearchDto() { WithDeleted = true, WithStacked = true, TrashedAfter = TrashedAfter }).Where(x => x.IsTrashed).Select(x => x.Id);
             await foreach (var itemImmichChank in allAssetsImmich.Chunk(CHANK_SIZE))
             {
-                var syncItems = await _syncDB.SyncItems.Where(x => itemImmichChank.Contains(x.ImmichKey)).ToListAsync();
+                var syncItems = await db.SyncItems.Where(x => itemImmichChank.Contains(x.ImmichKey)).ToListAsync();
                 var googleKeys = syncItems.Select(x => x.GoogleKey).ToList();
 
                 var existingInGoogle = await _gpmcClient.IntersectKeys(googleKeys).ToListAsync();
@@ -86,14 +85,14 @@ namespace Immich.ToGPhoto.App
                 logger.LogInformation("Удалены {count} элементов в Google Photo которые в Immich лежат в корзине", removedFromGoogle.Count);
                 var syncItemsToRemove = syncItems.Where(x => removedFromGoogle.Contains(x.GoogleKey));
 
-                _syncDB.SyncItems.RemoveRange(syncItemsToRemove);
-                await _syncDB.SaveChangesAsync();
+                db.SyncItems.RemoveRange(syncItemsToRemove);
+                await db.SaveChangesAsync();
             }
         }
         /// <summary>
         /// Гарантия что все фото загружены
         /// </summary>
-        private async Task LoadPhotos(ILogger logger)
+        private async Task LoadPhotos(SyncDB db, ILogger logger)
         {
             var allAssetsImmich = _immichClient.SearchAllAssetsAsync(new MetadataSearchDto() { WithDeleted = false, WithStacked = true, Order = AssetOrder.Asc });
 
@@ -102,7 +101,7 @@ namespace Immich.ToGPhoto.App
             await foreach (var itemImmichChank in allAssetsImmich.Chunk(CHANK_SIZE))
             {
                 // Те элементы которые вроде как загружены
-                var keysDBUploaded = await _syncDB.SyncItems.Where(x => itemImmichChank.Select(x => x.Id).Contains(x.ImmichKey)).ToListAsync();
+                var keysDBUploaded = await db.SyncItems.Where(x => itemImmichChank.Select(x => x.Id).Contains(x.ImmichKey)).ToListAsync();
                 // Те элементы которые в действительности загружены в Google Photos. (Синхронизация с Google Photos)
                 var toUpload = await _gpmcClient.IntersectKeys(keysDBUploaded.Select(x => x.GoogleKey)).ToHashSetAsync();
                 // Те элементы которые были случайно удалены в гугл фото
@@ -111,8 +110,8 @@ namespace Immich.ToGPhoto.App
                 if (toResetUpload.Any())
                 {
                     logger.LogInformation("Обнаружены удалённые элементы в Google Photo которые присутствуют в Immich");
-                    _syncDB.SyncItems.RemoveRange(toResetUpload);
-                    await _syncDB.SaveChangesAsync();
+                    db.SyncItems.RemoveRange(toResetUpload);
+                    await db.SaveChangesAsync();
                 }
                 // Обнаружение фото требующих загрузки в Google Photos.
                 var toNewUpload = itemImmichChank.ExceptBy(keysDBUploaded.Select(x => x.ImmichKey), x => x.Id).OrderBy(x => x.ExifInfo?.DateTimeOriginal).Select(x => x.Id);
@@ -129,12 +128,12 @@ namespace Immich.ToGPhoto.App
 
                     logger.LogInformation("В Google Photo успешно загружено {count} элементов", resultUpload.Count);
 
-                    await CheckConstraintConfict(resultUpload);
+                    await CheckConstraintConfict(db, resultUpload);
 
                     var addDBItems = resultUpload.Select(x => new SyncItemModel() { GoogleKey = x.Value, ImmichKey = mapFilesKey[x.Key] });
 
-                    await _syncDB.SyncItems.AddRangeAsync(addDBItems);
-                    await _syncDB.SaveChangesAsync();
+                    await db.SyncItems.AddRangeAsync(addDBItems);
+                    await db.SaveChangesAsync();
 
                     Directory.Delete(pathFilesUpload, true);
 
@@ -145,9 +144,9 @@ namespace Immich.ToGPhoto.App
             }
         }
 
-        private async Task CheckConstraintConfict(IDictionary<Uri, string> resultUpload)
+        private async Task CheckConstraintConfict(SyncDB db, IDictionary<Uri, string> resultUpload)
         {
-            await using var conflictItems = GetConflictItems(resultUpload).GetAsyncEnumerator();
+            await using var conflictItems = GetConflictItems(db, resultUpload).GetAsyncEnumerator();
             if (await conflictItems.MoveNextAsync())
             {
                 StringBuilder stringBuilder = new("Найдены дубликаты в Immich которые в Google Photo помечены одним ключом\n");
@@ -160,9 +159,9 @@ namespace Immich.ToGPhoto.App
             }
         }
 
-        private async IAsyncEnumerable<(string fileName, string googleKey)> GetConflictItems(IDictionary<Uri, string> resultUpload)
+        private async IAsyncEnumerable<(string fileName, string googleKey)> GetConflictItems(SyncDB db, IDictionary<Uri, string> resultUpload)
         {
-            foreach (var item in _syncDB.SyncItems.Where(x => resultUpload.Values.Contains(x.GoogleKey)))
+            await foreach (var item in db.SyncItems.Where(x => resultUpload.Values.Contains(x.GoogleKey)).AsAsyncEnumerable())
             {
                 var fileInfo = await _immichClient.GetAssetInfoAsync(item.ImmichKey);
                 yield return (fileInfo.OriginalFileName, item.GoogleKey);
@@ -261,10 +260,6 @@ namespace Immich.ToGPhoto.App
             return (uploadFolder, mapping);
         }
 
-        public void Dispose()
-        {
-            _syncDB.Dispose();
-            GC.SuppressFinalize(this);
-        }
+        private SyncDB GetDB() => new(Key);
     }
 }
